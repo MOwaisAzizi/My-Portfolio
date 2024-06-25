eb933f2e96a897e313a43234e02b3d6ef3294fb6
owias azizi <owaisazizi@jmail.com>
2024-06-25 10:36:53 +0430

    adding some changing files

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Allcomponents from "./Components/allcomponents";
+import Main from "./Components/main/main";
+import Project from "./Components/main/mmain/instances/instanceProjects/project";
+import UseDarkMode from "./Dark-mode";
+
+
 
 const App = () => {
-    return (
-        <div className="app">
+
+    const [theme, toggleTheme] = UseDarkMode()
+
+    let content = (
+        <div className='app'
+            style={{
+                background: theme == 'dark' ? 'rgba(0, 0, 0, 0.8)' : 'rgb(235, 225, 225)',
+                color: theme == 'dark' ? 'white' : 'black'
+            }}>
+            {
+                theme == 'dark' ?
+                    <i onClick={toggleTheme}><a className="fa fa-sun icon2" ></a></i>
+                    :
+                    <i onClick={toggleTheme}><a className="fa fa-moon icon1" ></a></i>
+            }
+
             <Router>
-                <Allcomponents />
+                <Routes>
+                    <Route path="/" element={<Main theme={theme} />}></Route>
+                    <Route path="/project" element={<Project />}  ></Route>
+                </Routes>
             </Router>
         </div>
+
+    )
+
+    return (
+        content
     )
 }
 export default App
--- src/Components/main/main.js
+++ src/Components/main/main.js
@@ -20,12 +20,13 @@ import FaContact from '../links/contacts/Facontact';
 import Faabout from '../links/about/Faabout';
 import FaExprence from '../links/exprence/Faexprence';
 import { FaArrowDown } from 'react-icons/fa';
-
+import UseDarkMode from "../../Dark-mode";
+import 'font-awesome/css/font-awesome.min.css'
 
 
 function Main(props) {
   const [isenglish, setisenglish] = useState(true)
-  const [isopen,setisopen] = useState(false);
+  const [isopen, setisopen] = useState(false)
 
   const handlelangeage = () => {
     setisenglish(!isenglish)
@@ -44,61 +45,67 @@ function Main(props) {
   const scrollToSection = (elementRef) => {
     window.scrollTo({
       top: elementRef.current.offsetTop,
-      behavior:'smooth'
-
+      behavior: 'smooth'
     })
   }
+
+  const [theme, toggleTheme] = UseDarkMode()
 
   return (
 
     <div className='fullpage'>
       {
         isenglish ?
-            <nav className='navbar'>
-                    <ul className={isopen ? 'navbar-links active ' : 'navbar-links'}>
-                      <li  onClick={() => scrollToSection(home)}>Home</li>
-                      <li  onClick={() => scrollToSection(instace)}>Projects</li>
-                      <li onClick={() => scrollToSection(about)}>about</li>
-                      <li  onClick={() => scrollToSection(exprence)}>Exprence</li>
-                      <li  onClick={() => scrollToSection(ablity)}>Skills</li>
-                      <li  onClick={() => scrollToSection(contact)}>Contact</li>
-                    </ul>
+          <nav className='navbar'
+            style={{
+              background: props.theme == 'dark' ? 'rgb(128, 128, 128)' : 'rgb(211, 216, 211)',
+              color: props.theme == 'dark' ? 'white' : 'black'
+            }}>
+            <ul className={isopen ? 'navbar-links active ' : 'navbar-links'}>
+              <li onClick={() => scrollToSection(home)}>Home</li>
+              <li onClick={() => scrollToSection(instace)}>Projects</li>
+              <li onClick={() => scrollToSection(about)}>about</li>
+              <li onClick={() => scrollToSection(exprence)}>Exprence</li>
+              <li onClick={() => scrollToSection(ablity)}>Skills</li>
+              <li onClick={() => scrollToSection(contact)}>Contact</li>
+            </ul>
 
-                    <div className='navbartoggle' onClick={toggleNav}>
-                      <i  className={isopen ? 'fa fa-times time' : 'fa fa-bars'}></i>
-                    </div>
-            </nav>
-          
-           :
+            <div className='navbartoggle' onClick={toggleNav}>
+              <i className={isopen ? 'fa fa-times time' : 'fa fa-bars'}></i>
+            </div>
+          </nav>
 
-          
-           <nav className='navbar '>
-           <ul className={isopen ? 'navbar-links active ' : 'navbar-links'}  >
-             <li  onClick={() => scrollToSection(home)}>خانه</li>
-             <li  onClick={() => scrollToSection(instace)}>پروژه ها</li>
-             <li onClick={() => scrollToSection(about)}>درباره</li>
-             <li  onClick={() => scrollToSection(exprence)}>تجارب</li>
-             <li  onClick={() => scrollToSection(ablity)}>مهارت ها</li>
-             <li  onClick={() => scrollToSection(contact)}>ارتباط</li>
-           </ul>
+          :
 
-           <div className='navbartoggle' onClick={toggleNav}>
-             <i  className={isopen ? 'fa fa-times time' : 'fa fa-bars'}></i>
-           </div>
-   </nav>
-          
+
+          <nav className='navbar '>
+            <ul className={isopen ? 'navbar-links active ' : 'navbar-links'}  >
+              <li onClick={() => scrollToSection(home)}>خانه</li>
+              <li onClick={() => scrollToSection(instace)}>پروژه ها</li>
+              <li onClick={() => scrollToSection(about)}>درباره</li>
+              <li onClick={() => scrollToSection(exprence)}>تجارب</li>
+              <li onClick={() => scrollToSection(ablity)}>مهارت ها</li>
+              <li onClick={() => scrollToSection(contact)}>ارتباط</li>
+            </ul>
+
+            <div className='navbartoggle' onClick={toggleNav}>
+              <i className={isopen ? 'fa fa-times time' : 'fa fa-bars'}></i>
+            </div>
+          </nav>
+
       }
 
-    {
-            isenglish ?
-              <button onClick={handlelangeage} className='btn-change fa ' >فا</button>
-              :
-              <button onClick={handlelangeage} className=' btn-change en' >En</button>
-          }
+
+      {
+        isenglish ?
+          <button onClick={handlelangeage} className='btn-change fa ' >فا</button>
+          :
+          <button onClick={handlelangeage} className=' btn-change en' >En</button>
+      }
 
       <Container className='main' data-aos="fade-in" data-aos-delay="200" ref={home}>
 
-        <div id='section1'  onClick={()=>setisopen(false)}>
+        <div id='section1' onClick={() => setisopen(false)}>
           <Row>
             <Col lg={4} xl={4}>
               {isenglish ?
@@ -111,21 +118,21 @@ function Main(props) {
               <Middlemain >
                 {isenglish ?
                   <div className='info'>
-                    <h2 className='pt-2 ms-2 ms-sm-0'  style={{textAlign:'center'}}><b>Mohammad Owais Azizi</b></h2>
+                    <h2 className='pt-2 ms-2 ms-sm-0' style={{ textAlign: 'center' }}><b>Mohammad Owais Azizi</b></h2>
                     <p className='p-1 ms-3 ms-sm-0' >Living in Herat city, pursuing studies in Computer Sience in Herat university.
-                     Should you require assistance? please feel free to contact me through any of the provided ways. <br />
+                      Should you require assistance? please feel free to contact me through any of the provided ways. <br />
                       <div className='phone mb-3 mb-md-0' >
                         <span onClick={() => scrollToSection(contact)}> <button className="mb-2 mt-2 contactbtn" data-aos="fade-left" data-aos-delay="800"
-                        >Contact me <FaArrowDown/></button></span>
+                        >Contact me <FaArrowDown /></button></span>
                       </div>
                     </p>
                   </div> :
 
                   <div className='info'>
-                    <h2 className='pt-2 ms-2 ms-sm-0'  style={{textAlign:'center'}}><b>محمد اویس عزیزی</b></h2>
+                    <h2 className='pt-2 ms-2 ms-sm-0' style={{ textAlign: 'center' }}><b>محمد اویس عزیزی</b></h2>
                     <p className='p-1 ms-3 ms-sm-0' >در شهر هرات زندگی میکند و در حال آموختن رشته کمپیوتر ساینس در دانشگاه هرات است. میتوانم برای شما کمک کنم ؟ اگر بلی میتوانید از راه های زیر با من تماس بگیرید <br />
                       <div className='phone mb-3 mb-md-0' >
-                        <span onClick={() => scrollToSection(contact)}> <button className="mb-2 mt-2 contactbtn" data-aos="fade-right" data-aos-delay="800"> <FaArrowDown/> ارتباط با من  </button></span>
+                        <span onClick={() => scrollToSection(contact)}> <button className="mb-2 mt-2 contactbtn" data-aos="fade-right" data-aos-delay="800"> <FaArrowDown /> ارتباط با من  </button></span>
                       </div>
                     </p>
                   </div>
@@ -141,16 +148,18 @@ function Main(props) {
               }
             </Col>
           </Row>
-        
+
         </div>
 
       </Container>
 
-      <div ref={instace}> {isenglish ? <Instances click={props.clicked} /> : <FaInstances click={props.clicked} />}</div>
-      <div ref={about}> {isenglish ? <About /> : <Faabout />}</div>
-      <div ref={exprence}> {isenglish ? <Exprence /> : <FaExprence />}</div>
-      <div ref={ablity}> {isenglish ? <Ablity /> : <FaAblity />}</div>
-      <div ref={contact}> {isenglish ? <Contact /> : <FaContact />}</div>
+      <div onClick={() => setisopen(false)}>
+        <div ref={instace}> {isenglish ? <Instances click={props.clicked} /> : <FaInstances click={props.clicked} />}</div>
+        <div ref={about}> {isenglish ? <About /> : <Faabout />}</div>
+        <div ref={exprence}> {isenglish ? <Exprence /> : <FaExprence />}</div>
+        <div ref={ablity}> {isenglish ? <Ablity /> : <FaAblity />}</div>
+        <div ref={contact}> {isenglish ? <Contact /> : <FaContact />}</div>
+      </div>
     </div>
   );
 }
