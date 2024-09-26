cbf667fa703b3510de7807fc85c5d821f4fa3631
owias azizi <owaisazizi@jmail.com>
2024-09-26 14:14:47 +0300

    update

--- src/Components/main/mmain/instances/instaces.js
+++ src/Components/main/mmain/instances/instaces.js
@@ -144,7 +144,7 @@ const Instances = ({theme}) => {
 
         <h2 className="pt-md-5 pt-lg-1 pt-4" style={{ textAlign: 'center' }} data-aos="fade-up">My  Projects Instances</h2>
 
-        <div className="slides" style={{ transform: `translateX(-${ width >= 700 ? currentIndex * 436 : width <700 && width>500 ? currentIndex * 520 : width <500 && width>400 ? currentIndex * 480 :currentIndex * 360}px)` }}>
+        <div className="slides" style={{ transform: `translateX(-${ width >= 700 ? currentIndex * 436 : width <700 && width>500 ? currentIndex * 520 : width <500 && width>400 ? currentIndex * 480 :currentIndex * 365}px)` }}>
          
 
           <div data-aos="fade-up" className="mb-3 slide" data-aos-duration="1000">
