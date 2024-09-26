2edaa27ba5077f185b0fb2d1563c59cb291e2a8c
owias azizi <owaisazizi@jmail.com>
2024-09-26 14:08:08 +0300

    add

--- src/Components/main/mmain/instances/Fainstances.js
+++ src/Components/main/mmain/instances/Fainstances.js
@@ -132,9 +132,9 @@ const FaInstances = (props) => {
 
    <Container className="contain">
    <div className="pb-3 slider">
-        <h2 className="pt-md-5 pt-lg-1 pt-4" style={{ textAlign: 'center' }} data-aos="fade-up">نمونه پروژه های من</h2>
+        <h2 className="pt-md-5 pt-lg-1 pt-4" style={{ textAlign: 'center' }} data-aos="fade-up">نمونه های پروژه های من</h2>
         {/* <Row> */}
-   <div  className="slides" style={{ transform: `translateX(-${ width >= 750 ? currentIndex * 450 : width <750 && width>500 ? currentIndex * 520 :  currentIndex * 345}px)` }}>
+   <div  className="slides" style={{ transform: `translateX(-${ width >= 700 ? currentIndex * 436 : width <700 && width>500 ? currentIndex * 520 : width <500 && width>400 ? currentIndex * 480 :currentIndex * 350}px)` }}>
 
 
           <div  data-aos="fade-up" className="mb-3 slide"  data-aos-duration="1000">
