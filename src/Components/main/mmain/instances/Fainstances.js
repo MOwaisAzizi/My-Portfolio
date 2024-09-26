85539ef7f0f7c1d3a439bdba228dc209be99655d
owias azizi <owaisazizi@jmail.com>
2024-09-26 13:40:29 +0300

    update

--- src/Components/main/mmain/instances/Fainstances.js
+++ src/Components/main/mmain/instances/Fainstances.js
@@ -135,7 +135,7 @@ const FaInstances = (props) => {
    <div className="pb-3 slider">
         <h2 className="pt-md-5 pt-lg-1 pt-4" style={{ textAlign: 'center' }} data-aos="fade-up">نمونه پروژه های من</h2>
         {/* <Row> */}
-   <div  className="slides" style={{ transform: `translateX(-${ width >= 750 ? currentIndex * 450 : width <750 && width>500 ? currentIndex * 520 :  currentIndex * 320}px)` }}>
+   <div  className="slides" style={{ transform: `translateX(-${ width >= 750 ? currentIndex * 450 : width <750 && width>500 ? currentIndex * 520 :  currentIndex * 330}px)` }}>
 
 
           <div  data-aos="fade-up" className="mb-3 slide"  data-aos-duration="1000">
