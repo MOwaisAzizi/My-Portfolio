898f43c10450ecc555c1fc6ca2a7368d7edea68e
owias azizi <owaisazizi@jmail.com>
2024-10-03 14:44:27 +0300

    add some

--- src/Components/links/about/Faabout.js
+++ src/Components/links/about/Faabout.js
@@ -31,7 +31,7 @@ const Faabout = (props) => {
                             من قبلا زیان انگلیسی را آموخته و بعد از آن وارد دانشگاه و موسسه واسا جهت یادگیری فرانت اند شدم و به درستی تمام اش کردم.
                             من علاقه شدید به یادگیری بیشتر توسعه وب از جمله بخش بک اند و تکنالوژی هوش مصنوعی دارم
                             .من قادر هستم که هر گونه ویب سایت و ویب اپلیکیشن را بسازم و علاقه مند به کار کردن روی پروژه های تیمی هستم.
-                            بر علاوه من بیشتر ده تصدیق نامه از موسسات مختلف دارم. من توسعه فرانت اند را در موسسه واسا تمام کردم و تصدیق نامه آنرا نیز دارم.
+                            بر علاوه من بیشتر ده تصدیق نامه از موسسات مختلف که تصدیق نامه توسعه فرانت اند موسسه واسا هم شامل آن میشود دارم.
                         </p>
                             <div className="cvdiv mt-3" data-aos="fade-left" data-aos-delay="300" data-aos-duration="1000">
                                 <button style={{background: props.theme === 'dark' ?'#02f5fd': '#8ef7cd',boxShadow:props.theme === 'dark' ? '2px 4px 10px  rgba(14, 205, 211,0.8)':'4px 8px 15px rgba(0,0,0,0.2)'}} className="mb-2 mt-2 btn-primary-cv py-2 px-3" ><a style={{color: props.theme === 'dark' ?'white': 'black',}} href={CV} download='CV' className="a-btn">دانلود سی وی <FaDownload /></a></button>
