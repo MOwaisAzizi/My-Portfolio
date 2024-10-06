5567f8438b84d28c8dc299ef1fbee774f0c818a8
owias azizi <owaisazizi@jmail.com>
2024-10-06 13:52:37 +0300

    add

--- src/Components/links/about/Faabout.js
+++ src/Components/links/about/Faabout.js
@@ -34,7 +34,7 @@ const Faabout = (props) => {
                             بر علاوه من بیشتر ده تصدیق نامه از موسسات مختلف که تصدیق نامه توسعه فرانت اند موسسه واسا هم شامل آن میشود دارم.
                         </p>
                             <div className="cvdiv mt-3" data-aos="fade-left" data-aos-delay="300" data-aos-duration="1000">
-                                <button style={{background: props.theme === 'dark' ?'#02f5fd': '#8ef7cd',boxShadow:props.theme === 'dark' ? '2px 4px 10px  rgba(14, 205, 211,0.8)':'2px 5px 15px 4px 12px rgba(0,0,0,0.2)'}} className="mb-2 mt-2 btn-primary-cv py-2 px-3" ><a style={{color: props.theme === 'dark' ?'white': 'black',}} href={CV} download='CV' className="a-btn">دانلود سی وی <FaDownload /></a></button>
+                                <button style={{background: props.theme === 'dark' ?'#02f5fd': '#8ef7cd',boxShadow:props.theme === 'dark' ? '2px 4px 10px  rgba(14, 205, 211,0.8)':'2px 4px 15px rgba(0,0,0,0.2)'}} className="mb-2 mt-2 btn-primary-cv py-2 px-3" ><a style={{color: props.theme === 'dark' ?'white': 'black',}} href={CV} download='CV' className="a-btn">دانلود سی وی <FaDownload /></a></button>
                             </div>
             </Container>
         </div>
