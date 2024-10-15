afe62ebeb80e708f6e1eb41d74e2fe4fa5a0fc75
owias azizi <owaisazizi@jmail.com>
2024-10-15 14:14:55 +0430

    add

--- src/Components/main/mmain/middlemain.js
+++ src/Components/main/mmain/middlemain.js
@@ -5,7 +5,7 @@ import './middlemain.css'
 const Middlemain = (props) => {
 
   return (
-    <div className='middlemain mt-0 ms-2 ms-sm-0 mt-lg-5 mb-lg-5 '>
+    <div className='middlemain mt-0 mt-md-1 mt-lg-5 ms-2 ms-sm-0  mb-lg-5 '>
      <div className='pic ' style={{boxSizing:'border-box'}}>
      <img src={owais}/>
      </div>
